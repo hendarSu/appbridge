89caaaec28f30bebe9fe042a235c8fdd9957bb81
Hendar <hendarsundana@gmail.com>
2023-11-01 12:31:05 +0700

    fix: deploy

--- pages/register/index.jsx
+++ pages/register/index.jsx
@@ -27,62 +27,68 @@ export default function Register() {
 
   return (
     <Layout>
-      <div className={styles.container}>
-        <div className="mb-5">
-          <p className={styles.title}>
-            Register <a href="#">WooBridge!</a>
-          </p>
-        </div>
-        <div className="card p-1">
-          <div className="card-body">
-            <form onSubmit={handleSubmit}>
-              <div className="mb-3">
-                <label for="exampleInputEmail1" className="form-label">
-                  Email address
-                </label>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
+      <div className="container">
+        <div className="row justify-content-center align-items-center" style={{ height: '100vh' }}>
+          <div className="col-md-4"></div>
+          <div className="col-md-4">
+            <div className="mb-5 text-center">
+              <p className="h1 font-w700">
+                Register <a href="#">WooBridge!</a>
+              </p>
+            </div>
+            <div className="card p-1">
+              <div className="card-body">
+                <form onSubmit={handleSubmit}>
+                  <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">
+                      Email address
+                    </label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="exampleInputPassword1"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">
+                      Konfirmasi Password
+                    </label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="exampleInputPassword1"
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                  </div>
+                  <div className="d-flex">
+                    <Button
+                      type="submit"
+                      className="btn btn-primary flex-row-reverse"
+                    >
+                      Register
+                    </Button>
+                  </div>
+                </form>
               </div>
-              <div className="mb-3">
-                <label for="exampleInputPassword1" className="form-label">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="exampleInputPassword1"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <div className="mb-3">
-                <label for="exampleInputPassword1" className="form-label">
-                  Konfirmasi Password
-                </label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="exampleInputPassword1"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-              </div>
-              <div className="d-flex">
-                <Button
-                  type="submit"
-                  className="btn btn-primary flex-row-reverse"
-                >
-                  Register
-                </Button>
-              </div>
-            </form>
+            </div>
           </div>
+          <div className="col-md-4"></div>
         </div>
       </div>
 
